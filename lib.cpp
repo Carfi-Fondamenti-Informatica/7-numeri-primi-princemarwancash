483c9421b26fd805070d8b41c17fcd008ac92e38
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-11-30 23:41:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#include "lib.h"
+int fun (int a){
+    int  div=1, divInt=0;
+    while(divInt<=1 and div<=a/2) {
+        if(a%div==0){
+            divInt++;
+        }
+        div++;
+    } if (divInt==1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
