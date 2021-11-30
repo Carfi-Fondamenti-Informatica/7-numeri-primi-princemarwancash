db57571f2a9622c487870b2286d0ada0c50a2213
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-11-30 23:42:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int a=0;
+    int risultato;
+    cin >> a;
+    risultato = fun(a);
+    if (risultato==1){
+        cout << "numero primo" << endl;
+    }else{
+        cout << "numero non primo" << endl;
+    }
+    return 0;
 }
